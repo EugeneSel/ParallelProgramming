8dfd9958172d80dbf1234e59bcd839497b5c307d
Sielskyi Yevhenii <yevhenii.sielskyi@ensta-paris.fr>
2020-01-11 13:01:51 +0100

    Nothing special

--- project/ant_simu.cpp
+++ project/ant_simu.cpp
@@ -133,7 +133,7 @@ int main(int nargs, char* argv[])
         # endif
 
         // the end condition:
-        if (food_quantity >= 100) {
+        if (food_quantity >= 10) {
             // end general clock:
             end_general = chrono::system_clock::now();
 
